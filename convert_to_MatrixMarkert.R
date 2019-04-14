49661505fb6606032730e9247fb6e616a07eb897
FloWuenne <flowuenne@gmail.com>
2019-04-14 23:58:01 +0000

    Updated R script for converting to MM format.

--- convert_to_MatrixMarkert.R
+++ convert_to_MatrixMarkert.R
@@ -1,7 +1,15 @@
+#!/usr/bin/env Rscript 
+
+print("Converting matrixto MatrixMarket format...")
+
 library(Matrix)
+library(data.table)
 ## Read in salmon alevin matrix output
 
 # save as sparse matrix
+salmon_matrix <- fread("./alevin_output/alevin/quants_mat.gz")
 salmon_matrix.sparse  <- Matrix(salmon_matrix , sparse = T )
 
-writeMM(obj = salmon_matrix.sparse , file="sparse_matrix.mtx")
+writeMM(obj = salmon_matrix.sparse , file="./sparse_matrix.mtx")
+
+print("Done converting matrix!")

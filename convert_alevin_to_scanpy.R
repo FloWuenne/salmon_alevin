a8bbc69b5eb676ed9d64c0251ccba676db24cccf
FloWuenne <flowuenne@gmail.com>
2019-04-29 18:40:40 +0000

    Added MIT License.

---
+++ convert_alevin_to_scanpy.R
@@ -0,0 +1,48 @@
+## How to run tool
+# Rscript alevin_to_seurat.R --input $alevin_matrix_rds  --output $seurat_object
+
+# Set up R error handling to go to stderr
+options(show.error.messages=F, error=function(){cat(geterrmessage(),file=stderr());q("no",1,F)})
+
+# Avoid crashing Galaxy with an UTF8 error on German LC settings
+loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+
+
+suppressPackageStartupMessages({
+  library("getopt")
+  library("Seurat")
+})
+
+# Import required libraries
+options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+
+# Take in trailing command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Get options using the spec as defined by the enclosed list
+# Read the options from the default: commandArgs(TRUE)
+option_specification = matrix(c(
+  'input', 'i1', 2, 'character',
+  'output', 'o', 2, 'character'
+), byrow=TRUE, ncol=4);
+
+# Parse options
+options = getopt(option_specification);
+
+# Print options to stderr
+# Useful for debugging
+#cat("\n input file: ",options$input)
+#cat("\n output file: ",options$output)
+
+## Read in alevin sparse Matrix as RDS file
+alevin_matrix <- readRDS(options$input)
+
+## Create Seurat object
+seurat_object <- CreateSeuratObject(alevin_matrix)
+
+anndata <- Convert(from = seurat_object, to = "anndata", filename = "anndata_object.h5ad")
+
+# Output kmer counts
+saveRDS(seurat_object, file = options$output)
+
+cat("\n Successfully created Scanpy object! \n")
